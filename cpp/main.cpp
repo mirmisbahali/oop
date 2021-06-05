7acd4cc799403fb39241e77155097354614c3778
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-06-05 06:44:38 +0530

    constructors in c++

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -14,14 +14,21 @@ public:
         cout << "Company - " << Company << endl;
         cout << "Age - " << Age << endl;
     }
+
+    Employee(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
 };
 
 int main(void)
 {
-    Employee employee1;
-    employee1.Name = "Mark";
-    employee1.Company = "boringcompany";
-    employee1.Age = 30;
+    Employee employee1 = Employee("Mark", "boringcompany", 30);
     employee1.IntroduceYourself();
+
+    Employee employee2 = Employee("John", "amazon", 32);
+    employee2.IntroduceYourself();
+
     return 0;
 }
